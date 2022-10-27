2d88482e9262910771836a5fd927c9da385b72ad
sourav-panda01 <souravpanda601@gmail.com>
2022-10-27 18:12:51 +0530

    Task1 Signup Html basic form completed

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const cors=require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+// sequelize
+//    //.sync({ force: true })
+//   .sync()
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
